fe8870605416205e630624779ce93b4647363a60
Yarre <yaroslav.syvukha@nure.ua>
2022-08-21 23:20:42 +0300

    Improved all

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
+from random import seed
+from random import randint
 import discord
 
 
 intents = discord.Intents.all()
 intents.message_content = True
 client = discord.Client(intents=intents)
+seed(1)
+
+nicks = ['uyuyuy', 'Yarre']
+games = ['dota 2', 'Wallpaper Engine']
+data = ['uyuyuy', False]
+messages = ['@everyone , {0} goes {1}. Go with him!',
+            '@everyone  I({0}) want to play {1}. Go?']
+
+
+def getMessage(nick: str, game: str):
+    num = randint(0, len(messages) - 1)
+    print(num)
+    message = messages[num]
+    return message.replace('{0}', nick).replace('{1}', game)
 
 
 @client.event
@@ -12,9 +28,46 @@ async def on_ready():
 
 
 @client.event
-async def on_presence_update(prev, cur: discord.Member):
-    if cur.name == "uyuyuy" and cur.activity.name == 'Dota 2':
-        channel = client.get_channel(942895908981473307)
-        await channel.send('@everyone Dota<3')
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
 
-client.run('NTA1Mzg1Mjc2MjczODUyNDM0.GsC6el.OBiqORd9SFrwkatuqdN_VNH3jnxN6HcwcqaJeM')
+    input = message.content
+
+    if input.startswith('!h'):
+        await message.channel.send(f'Command: d - Data, an - Add Nick, ag - Add Game')
+
+    if input.startswith('!d'):
+        await message.channel.send(nicks)
+        await message.channel.send(games)
+        await message.channel.send(data)
+
+    if input.startswith('!an'):
+        nicks.append(input.split(' ')[1])
+
+    if input.startswith('!ag'):
+        games.append(input.split(' ')[1])
+
+@client.event
+async def on_presence_update(prev, cur: discord.Member):
+    if cur.activity == None:
+        if cur.name:
+            data[1] = False
+
+        return
+
+    nick = cur.name
+    game = cur.activity.name
+
+    print(f'{nick} - {game}')
+
+    if nick in nicks and game in games:
+        if data[1] == True:
+            return
+
+        channel = client.get_channel(942895908981473307)
+        await channel.send(getMessage(nick, game))
+        data[0] = nick
+        data[1] = True
+
+client.run('')
